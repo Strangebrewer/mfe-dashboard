35fd5de393e1f8f32c0ae67c850a51622accce58
Strangebrewer <borrowed.carbon.atoms@gmail.com>
2026-03-23 01:08:22 -0600

    update webpack to use mfe-utils

--- webpack.config.ts
+++ webpack.config.ts
@@ -3,55 +3,23 @@ import 'webpack-dev-server';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import path from 'path';
 import webpack from 'webpack';
+import { createWebpackConfig, defaultShared } from '@bka-stuff/mfe-utils';
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const APP_NAME = 'mfe_dashboard';
 
-const config: Configuration = {
-  mode: 'development',
-  entry: './src/index.ts',
+const config = {
+  ...createWebpackConfig({
+    appName: APP_NAME,
+    resolve: path.resolve,
+    _dirname: __dirname,
+    port: 3001,
+  }),
 
-  output: {
-    publicPath: 'auto',
-    uniqueName: APP_NAME,
-    chunkLoadingGlobal: `webpackChunk_${APP_NAME}`,
-    crossOriginLoading: 'anonymous',
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js',
-    clean: true,
-  },
-
-
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
-    symlinks: true,
-    alias: {
-      axios: path.resolve(__dirname, "node_modules/axios"),
-      react: path.resolve(__dirname, "node_modules/react"),
-      "react-dom": path.resolve(__dirname, "node_modules/react-dom"),
-    },
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: {
-          loader: 'ts-loader',
-          options: {
-            configFile: path.resolve(__dirname, "tsconfig.json"),
-            transpileOnly: true,
-          }
-        },
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
-      }
-    ],
-  },
-
-  plugins: [
+    plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
     }),
@@ -63,27 +31,9 @@ const config: Configuration = {
         './App': './src/App',
       },
 
-      shared: {
-        react: { singleton: true },
-        'react-dom': { singleton: true },
-        'react-router-dom': { singleton: true },
-        "@tanstack/react-query": { singleton: true },
-        zustand: { singleton: true },
-        '@bka-stuff/mfe-utils': { singleton: true },
-      },
+      shared: defaultShared,
     }),
   ],
-
-  devServer: {
-    port: 3001,
-    hot: false,
-    historyApiFallback: true,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-      'Access-Control-Allow-Headers': '*',
-    },
-  },
-};
+} as Configuration;
 
 export default config;
